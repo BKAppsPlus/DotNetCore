ca38ff598502b2d33029b1e5f25a983927e83a7a
bkashefipour <43657153+bkashefipour@users.noreply.github.com>
2019-09-13 14:53:36 -0700

    Commit 0

---
+++ ConsoleApp001/ConsoleApp001/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
